fa4b2acd681726f41a832843a9f43a8dcdf1773f
isisgoncalves <isisvow@gmail.com>
2024-03-02 08:52:26 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main()
+    {
+        string[] velocidades = Console.ReadLine().Split(' ');
+        int vmin = int.Parse(velocidades[0]);
+        int vmax = int.Parse(velocidades[1]);
+
+        string comandos = Console.ReadLine();
+
+        Robo robo = new Robo(vmin, vmax);
+
+        foreach (char comando in comandos)
+        {
+            if (comando == 'A')
+            {
+                robo.Acelerar();
+            }
+            else if (comando == 'D')
+            {
+                robo.Desacelerar();
+            }
+        }
+
+        Console.WriteLine(robo.VelocidadeAtual);
+    }
+}
